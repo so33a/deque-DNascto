3772a14bbd991a176f8ecd320192240d1dccedcf
DNascto <dgn.nascto@gmail.com>
2016-09-08 22:42:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
-#include "fila.h"
+#include "fila.c"
 
 int main () {
-  FILA alunos = novaFila();
-  inserir(alunos, 9);
-  inserir(alunos, 8);
-  inserir(alunos, 3);
-  inserir(alunos, 10);
+    FILA alunos = novaFila();
+    inserirNoFinal(alunos, 9);
+    inserirNoFinal(alunos, 8);
+    inserirNoFinal(alunos, 3);
+    inserirNoFinal(alunos, 10);
 
-  imprimirFila(alunos);
+    imprimirFila(alunos);
 
-  while(!filaVazia(alunos))
-    printf ("removido: %d \n", remover(alunos));
+    while(!filaVazia(alunos))
+        printf ("removido: %d \n", removerDoComeco(alunos));
 
 
-  inserir(alunos, 10);
-  inserir(alunos, 32);
-  
-  imprimirFila(alunos);
+    inserirNoFinal(alunos, 10);
+    inserirNoFinal(alunos, 32);
 
-  destroiFila(alunos);
-  return 0;
+    imprimirFila(alunos);
+
+    destroiFila(alunos);
+
+    printf("Nova Fila\n\n");
+
+    alunos = novaFila();
+    inserirNoComeco(alunos, 1);
+    inserirNoComeco(alunos, 5);
+    inserirNoComeco(alunos, 9);
+    inserirNoComeco(alunos, 7);
+    inserirNoComeco(alunos, 3);
+
+    imprimirFila(alunos);
+
+    while(!filaVazia(alunos))
+        printf ("removido: %d \n", removerDofim(alunos));
+
+    inserirNoComeco(alunos, 12);
+    inserirNoComeco(alunos, 45);
+    inserirNoComeco(alunos, 78);
+
+    imprimirFila(alunos);
+    return 0;
 }
